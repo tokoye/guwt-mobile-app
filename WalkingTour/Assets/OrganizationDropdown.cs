2a01f0455626b7c221cf5623a7867361b67ecfaa
nick Latham <nlatham@zagmail.gonzaga.edu>
2020-10-14 17:28:24 -0700

    feat: added some UI for the initial view as well as some scripts

---
+++ WalkingTour/Assets/OrganizationDropdown.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// This namespace is required for using any UI components
+using UnityEngine.UI;
+//  This namespace is required to use the SceneManager
+using UnityEngine.SceneManagement;
+
+public class OrganizationDropdown : MonoBehaviour
+{
+    public Dropdown drop;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void dropdownClicked()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
