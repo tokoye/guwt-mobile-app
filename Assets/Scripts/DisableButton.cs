a5ab368771041459cf328cadaa2a343035a409c9
Spencer Jacobs <spencer.jacobs.sj13@gmail.com>
2021-02-23 11:43:37 -0800

    Significant UI changes, added some button onClick actions

---
+++ Assets/Scripts/DisableButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class DisableButton : MonoBehaviour
+{
+    public Button beginTourButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        beginTourButton.GetComponent<Button>();
+        beginTourButton.interactable = false;
+    }
+
+    public void changeButton(bool tourSelected)
+    {
+        if (tourSelected == true)
+        {
+            beginTourButton.interactable = true;
+        }
+        else
+        {
+            beginTourButton.interactable = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+
+    }
+}
