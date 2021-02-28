1ae850812a9253dc947e85c9a38e42b11eb2e005
Spencer Jacobs <spencer.jacobs.sj13@gmail.com>
2021-02-28 15:13:43 -0800

    Database is responding.
    TODO: parse API response and fill list of organizations with said info.

---
+++ Assets/Scripts/Data.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Data
+{
+    public string name;
+    public string _id;
+    public string department;
+}
---
+++ Assets/Scripts/RequestOrgs.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using Newtonsoft.Json;
+ 
+public class RequestOrgs : MonoBehaviour
+{
+    private string url = "https://backend.gonzagatours.app/api/organizations";
+    Text testText;
+    public Button DatabaseButton;
+   
+    
+    public void generateRequest()
+    {
+        DatabaseButton = GameObject.Find("DatabaseButton").GetComponent<Button>();
+        StartCoroutine(GetText());
+    }
+
+    public IEnumerator GetText()
+    {
+        UnityWebRequest request= UnityWebRequest.Get(url);
+        request.SetRequestHeader("Authentication", "");
+
+        yield return request.SendWebRequest();
+
+        if (request.isNetworkError || request.isHttpError)
+        {
+            Debug.Log(request.error);
+        }
+        else
+        {
+            // Show results as text
+            Debug.Log(request.downloadHandler.text);
+
+            var response = JsonConvert.DeserializeObject<List<RequestOrgs>>(request.downloadHandler.text);
+            Debug.Log(response[1].name);
+
+            // Or retrieve results as binary data
+            //byte[] results = request.downloadHandler.data;
+        }
+    }
+}
