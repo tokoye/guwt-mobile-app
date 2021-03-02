2451c6fbd014bc279c57f9338d7a8dfa4a4aed2f
Spencer Jacobs <spencer.jacobs.sj13@gmail.com>
2021-03-01 17:29:54 -0800

    Organizations pull from database (kinda)

--- Assets/Scripts/RequestOrgs.cs
+++ Assets/Scripts/RequestOrgs.cs
@@ -4,23 +4,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.Networking;
 using Newtonsoft.Json;
- 
+
 public class RequestOrgs : MonoBehaviour
 {
     private string url = "https://backend.gonzagatours.app/api/organizations";
     Text testText;
     public Button DatabaseButton;
-   
-    
-    public void generateRequest()
+    public List<string> orgNames = new List<string>();
+    public List<Dropdown.OptionData> orgDropdownNames = new List<Dropdown.OptionData>();
+    public UnityWebRequest request;
+    public Dropdown drop;
+
+
+
+    public void Start()
     {
-        DatabaseButton = GameObject.Find("DatabaseButton").GetComponent<Button>();
+        drop = GameObject.Find("OrganizationDropdown").GetComponent<Dropdown>();
+        drop.ClearOptions();
         StartCoroutine(GetText());
     }
 
     public IEnumerator GetText()
     {
-        UnityWebRequest request= UnityWebRequest.Get(url);
+        request = UnityWebRequest.Get(url);
         request.SetRequestHeader("Authentication", "Api-Key b49aad95-4493-44ff-b18a-be8bb2d1e012");
 
         yield return request.SendWebRequest();
@@ -32,13 +38,33 @@ public class RequestOrgs : MonoBehaviour
         else
         {
             // Show results as text
-            Debug.Log(request.downloadHandler.text);
-
+            //Debug.Log(request.downloadHandler.text);
             var response = JsonConvert.DeserializeObject<OrgData>(request.downloadHandler.text);
-            Debug.Log(response.data[2].name);
 
-            // Or retrieve results as binary data
-            //byte[] results = request.downloadHandler.data;
+            Dropdown.OptionData fillData = new Dropdown.OptionData();
+            fillData.text = response.data[0].department;
+            //orgDropdownNames.Add(fillData);
+            drop.options.Add(fillData);
+            fillData.text = response.data[1].department;
+            //orgDropdownNames.Add(fillData);
+            drop.options.Add(fillData);
+            fillData.text = response.data[2].department;
+            //orgDropdownNames.Add(fillData);
+            drop.options.Add(fillData);
+
+            Debug.Log(response.data[0].department);
+            Debug.Log(response.data[1].department);
+            Debug.Log(response.data[2].department);
+
+
+            populateOrgsDropdown(orgDropdownNames);
         }
     }
+
+    public void populateOrgsDropdown(List<Dropdown.OptionData> orgDropdownNames)
+    {
+
+    }
+
+
 }
