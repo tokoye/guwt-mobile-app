764f43ae21c056a78d7ea2196f1d7492584cf638
nick Latham <nlatham@zagmail.gonzaga.edu>
2021-03-04 18:12:44 -0800

    Revert "fix: merging"
    This reverts commit 112b4fc96b5387b9f16759c3edd07f46faae43cb.

---
+++ Assets/Scripts/DisableButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//[RequireComponent(typeof(Button))]
+public class beginTourButton : MonoBehaviour
+{
+    public Button tourButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tourButton = GameObject.Find("beginTourButton").GetComponent<Button>();
+        tourButton.interactable = false;
+    }
+
+    public void changeButton(bool tourSelected)
+    {
+        if (tourSelected == true)
+        {
+            tourButton.interactable = true;
+        }
+        else
+        {
+            tourButton.interactable = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+
+    }
+}
---
+++ Assets/Scripts/OrganizationDropdown.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// This namespace is required for using any UI components
+using UnityEngine.UI;
+//  This namespace is required to use the SceneManager
+using UnityEngine.SceneManagement;
+
+public class OrganizationDropdown : MonoBehaviour
+{
+    List<string> stringOptions = new List<string>();
+    List<Dropdown.OptionData> optionDataOptions = new List<Dropdown.OptionData>();
+    public Dropdown drop;
+    public Dropdown.OptionData fillData;
+    public Button beginTourButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //connecting the dropdown and button to the UI elements
+        drop = GameObject.Find("OrganizationDropdown").GetComponent<Dropdown>();
+        beginTourButton = GameObject.Find("beginTourButton").GetComponent<Button>();
+        drop.ClearOptions();
+
+        //adding every option in the stringOptions list. Note: must become Dropdown.OptionData items first. 
+        foreach(string i in stringOptions)
+        {
+            fillData = new Dropdown.OptionData();
+            fillData.text = i;
+            optionDataOptions.Add(fillData);
+            drop.options.Add(fillData);
+        }
+
+    }
+
+
+
+
+    List<string> getOrganizations()
+    {
+        //TODO: connect to aws and get the list of organizations with tours nearby 
+        return new List<string> { };
+    }
+
+    void dropdownClicked(Dropdown item)
+    {
+
+    }
+}
