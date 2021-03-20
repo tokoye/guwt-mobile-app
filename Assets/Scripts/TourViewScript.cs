57002002dbb2b526026f9a5a23cd822f65bf970f
nick Latham <nlatham@zagmail.gonzaga.edu>
2021-03-20 11:52:14 -0700

    feat: added location marker

--- Assets/Scripts/TourViewScript.cs
+++ Assets/Scripts/TourViewScript.cs
@@ -6,10 +6,10 @@ using UnityEngine.UI;
 public class TourViewScript : MonoBehaviour
 {
     string url = "";
-    public double lat = 47.66685234815894;
+    public double lat = 47.66685234815894;      //initial latitude and longitude to center on
     public double lon = -117.40292486555248;
     LocationInfo li;
-    public int zoom = 14;
+    public int zoom = 19;
     public int mapWidth = 640;
     public int mapHeight = 650;
     public enum mapType { roadmap, satellite, hybrid, terrain};
@@ -20,9 +20,12 @@ public class TourViewScript : MonoBehaviour
     public bool resetMap = false;
 
     private bool loadingMap = false;
+
     private IEnumerator mapCoroutine;
+    private IEnumerator locationCoroutine;
 
     public Button resetButton;
+    public Text debugTest;
 
     //this handles getting the google map and placing it on the texture
     IEnumerator GetGoogleMap(double lat, double lon)
@@ -33,8 +36,10 @@ public class TourViewScript : MonoBehaviour
 
         for (int i = 0; i < markers.Length; i++)
         {
-            url += "&markers=color:red|label:G|" + markers[i];
+            url += "&markers=color:red|label:S|" + markers[i];
         }
+
+        url += "&markers=color:blue|label:Y|" + lat + "," + lon;
 
         loadingMap = true;
         //UnityWebRequest www = new UnityWebRequest(url);
@@ -43,18 +48,66 @@ public class TourViewScript : MonoBehaviour
         loadingMap = false;
         GameObject.Find("RawImage").GetComponent<RawImage>().texture = www.texture;
         StopCoroutine(mapCoroutine);
+    }
 
+    //this handles getting the user location
+    IEnumerator GetLocation()
+    {
+        //print("entering");
+        //debugTest.text = "Entering";
+        //if (!Input.location.isEnabledByUser)
+        //    yield break;
+        //debugTest.text = "Passed";
+
+        Input.location.Start();
+
+        int maxWait = 20;
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+        if (maxWait < 1)
+        {
+            debugTest.text = "Timed Out";
+            print("Timed out");
+            yield break;
+        }
+
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+            debugTest.text = "Unable to determine device location";
+            print("Unable to determine device location");
+            yield break;
+        }
+        else
+        {
+            debugTest.text = "Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude;
+            lat = Input.location.lastData.latitude;
+            lon = Input.location.lastData.longitude;
+            print("Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
+            resetMap = true;
+        }
+        Input.location.Stop(); 
     }
 
 
     // Start is called before the first frame update
     void Start()
     {
+        debugTest = GameObject.Find("DebugTest").GetComponent<Text>();
+        //debugTest.text = "worked";
+
         mapCoroutine = GetGoogleMap(lat, lon);
         StartCoroutine(mapCoroutine);
 
         resetButton = GameObject.Find("RefreshButton").GetComponent<Button>();
         resetButton.onClick.AddListener(onRefreshButtonClicked);
+
+
+        locationCoroutine = GetLocation();
+        StartCoroutine(locationCoroutine);
     }
 
 
