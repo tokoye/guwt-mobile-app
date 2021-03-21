f9e5cacbad44d60c24b60ad41a08bc771b994ff2
Spencer Jacobs <spencer.jacobs.sj13@gmail.com>
2021-03-21 14:41:47 -0700

    Updated initial view significantly.
    There are 10 button options for tours, as the organization will be limited in the number of tours they can deploy at any one time.  The tours API is called successfully, which allows us to access your information (i.e. stops, locations, etc.)

--- Assets/Scripts/RequestOrgs.cs
+++ Assets/Scripts/RequestOrgs.cs
@@ -10,7 +10,6 @@ public class RequestOrgs : MonoBehaviour
     private string orgUrl = "https://backend.gonzagatours.app/api/organizations";
     private string toursUrl = "https://backend.gonzagatours.app/tour/tours";
     private string APIKey = "Api-Key 6d924d5a-cfba-41cc-b21c-6aeabe874a86";
-    public Text testText;
     public Button DatabaseButton;
     public List<string> orgNames = new List<string>();
     public List<Dropdown.OptionData> orgDropdownNames = new List<Dropdown.OptionData>();
@@ -19,16 +18,26 @@ public class RequestOrgs : MonoBehaviour
     public Dropdown drop;
     public List<Dropdown.OptionData> listOptions;
 
+    //Buttons that are appear as each optional tour 
+    public Button firstTourButton;
+    public Button secondTourButton;
+    public Button thirdTourButton;
+    public Button fourthTourButton;
+    public Button fifthTourButton;
+    public Button sixthTourButton;
+    public Button seventhTourButton;
+    public Button eighthTourButton;
+    public Button ninthTourButton;
+    public Button tenthTourButton;
+
 
 
     public void Start()
     {
-        drop = GameObject.Find("OrganizationDropdown").GetComponent<Dropdown>();
-        drop.ClearOptions();
+        FindGameObjects();
         StartCoroutine(GetText());
         StartCoroutine(GetTours());
 
-        testText = GameObject.Find("ListViewText").GetComponent<Text>();
 
         //adding listener to dropdown, which responds appropriately when a selection is made
         drop.onValueChanged.AddListener(delegate
@@ -36,6 +45,25 @@ public class RequestOrgs : MonoBehaviour
             DropdownValueChanged(drop);
 
         });
+    }
+
+    //This function just locates the game objects on the screen and connects them to their respective
+    //names here, so that they can be changed and altered dynamically.
+    public void FindGameObjects()
+    {
+        drop = GameObject.Find("OrganizationDropdown").GetComponent<Dropdown>();
+        drop.ClearOptions();
+
+        firstTourButton = GameObject.Find("TourListItem1").GetComponent<Button>();
+        secondTourButton = GameObject.Find("TourListItem2").GetComponent<Button>();
+        thirdTourButton = GameObject.Find("TourListItem3").GetComponent<Button>();
+        fourthTourButton = GameObject.Find("TourListItem4").GetComponent<Button>();
+        fifthTourButton = GameObject.Find("TourListItem5").GetComponent<Button>();
+        sixthTourButton = GameObject.Find("TourListItem6").GetComponent<Button>();
+        seventhTourButton = GameObject.Find("TourListItem7").GetComponent<Button>();
+        eighthTourButton = GameObject.Find("TourListItem8").GetComponent<Button>();
+        ninthTourButton = GameObject.Find("TourListItem9").GetComponent<Button>();
+        tenthTourButton = GameObject.Find("TourListItem10").GetComponent<Button>();
     }
 
     //This function makes a call to the database and populates the Organization dropdown with those organizations.
@@ -93,6 +121,10 @@ public class RequestOrgs : MonoBehaviour
             Debug.Log("TOURS RESULT:    " + toursRequest.downloadHandler.text);
             var response = JsonConvert.DeserializeObject<OrgData>(toursRequest.downloadHandler.text);
 
+            List<string> allTourNames = new List<string>();
+            allTourNames.Add(response.data[0].name);
+            allTourNames.Add(response.data[1].name);
+            setTourButtons(allTourNames);
         }
     }
 
@@ -100,7 +132,12 @@ public class RequestOrgs : MonoBehaviour
     //This function recognizes and responds to a selection that is made from the dropdown menu. 
     void DropdownValueChanged(Dropdown change)
     {
-        testText.text = "New Value: " + change.value;
     }
 
+    //This function accepts a list of strings, which are the names of each tour. It takes those strings
+    //and sets the appropriate number of buttons to clickable and sets their title to the name of the tour.
+    public void setTourButtons(List<string> alltourNames)
+    {
+
+    }
 }
