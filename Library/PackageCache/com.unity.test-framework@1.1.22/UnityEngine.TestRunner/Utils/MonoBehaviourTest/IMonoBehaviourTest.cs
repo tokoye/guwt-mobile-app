b38895fd34ef7fd446192745455ded30c9433ba0
nick Latham <nlatham@zagmail.gonzaga.edu>
2021-03-24 13:48:07 -0700

    fix: hopefully fixed compiler errors

--- Library/PackageCache/com.unity.test-framework@1.1.22/UnityEngine.TestRunner/Utils/MonoBehaviourTest/IMonoBehaviourTest.cs
+++ Library/PackageCache/com.unity.test-framework@1.1.22/UnityEngine.TestRunner/Utils/MonoBehaviourTest/IMonoBehaviourTest.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:Library/PackageCache/com.unity.test-framework@1.1.22/UnityEngine.TestRunner/Utils/MonoBehaviourTest/IMonoBehaviourTest.cs
 namespace UnityEngine.TestTools
 {
     /// <summary>
@@ -10,16 +9,3 @@ namespace UnityEngine.TestTools
         bool IsTestFinished {get; }
     }
 }
-=======
-namespace UnityEngine.TestTools
-{
-    /// <summary>
-    /// An interface implemented by a MonoBehaviour test.
-    /// </summary>
-    public interface IMonoBehaviourTest
-    {
-        /// <summary>True when the test is considered finished.</summary>
-        bool IsTestFinished {get; }
-    }
-}
->>>>>>> 311c70c6976bc5ea11bafc9e95d3e0e4ee456de5:Library/PackageCache/com.unity.test-framework@1.1.20/UnityEngine.TestRunner/Utils/MonoBehaviourTest/IMonoBehaviourTest.cs
